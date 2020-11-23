337020e8d21c561f6108c720f494fc94a2d1bf1f
Savin Oleh <savin.project402@gmail.com>
2020-11-23 18:21:58 +0200

    Added Write/Read arguments

--- ConsoleKvadrat/Program.cs
+++ ConsoleKvadrat/Program.cs
@@ -11,6 +11,18 @@ namespace ConsoleKvadrat
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+            Console.WriteLine("decision quadratic equation (aX^2 + bX + c = 0)");
+            Console.WriteLine("Insert argument (a)");
+            string a = Console.ReadLine();
+            Console.WriteLine("Insert argument (b)");
+            string b = Console.ReadLine();
+            Console.WriteLine("Insert argument (c)");
+            string c = Console.ReadLine();
+
+            Console.WriteLine("quadratic equatio = ( " + a + "X^2 +" + b + "X + " + c + " = 0 )"   );
+            Console.ReadLine();
+
         }
     }
 }
