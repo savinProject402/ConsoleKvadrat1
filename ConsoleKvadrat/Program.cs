69508577eefc41b7038cb28d5cc16bab7f9d27d7
Savin Oleh <savin.project402@gmail.com>
2020-11-23 18:38:22 +0200

    Added calculating the roots of an equation

--- ConsoleKvadrat/Program.cs
+++ ConsoleKvadrat/Program.cs
@@ -11,17 +11,38 @@ namespace ConsoleKvadrat
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-
+            double number_A, number_B, number_C;
             Console.WriteLine("decision quadratic equation (aX^2 + bX + c = 0)");
             Console.WriteLine("Insert argument (a)");
-            string a = Console.ReadLine();
+            number_A = double.Parse(Console.ReadLine());
             Console.WriteLine("Insert argument (b)");
-            string b = Console.ReadLine();
+            number_B = double.Parse(Console.ReadLine());
             Console.WriteLine("Insert argument (c)");
-            string c = Console.ReadLine();
+            number_C = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("quadratic equatio = ( " + a + "X^2 +" + b + "X + " + c + " = 0 )"   );
-            Console.ReadLine();
+            Console.WriteLine("quadratic equatio = ( " + number_A + "X^2 +" + number_B + "X + " + number_C + " = 0 )"   );
+
+            double Discriminant = number_B * number_B - 4 * number_A * number_C;
+            if (Discriminant < 0)
+            {
+                Console.WriteLine("Discriminant less than zero. No roots");
+                Console.ReadLine();
+            }
+            if (Discriminant == 0)
+            {
+                double x = Math.Round(-number_B / (2 * number_A), 2);
+                Console.WriteLine("Discriminant is zero.The root (X) = " + x + "");
+                Console.ReadLine();
+            }
+            if (Discriminant > 0)
+            {
+                double x1 = Math.Round((-number_B - Math.Sqrt(Discriminant)) / (2 * number_A), 2);
+                double x2 = Math.Round((-number_B + Math.Sqrt(Discriminant)) / (2 * number_A), 2);
+                Console.WriteLine("The Discriminant is " + Discriminant + ". The first root is" + x1 + ". The second root is" + x2 + "");
+
+                Console.ReadLine();
+            }
+
 
         }
     }
